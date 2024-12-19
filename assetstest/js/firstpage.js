12d3f865951ec02487ef581673bded0eee7fc6a8
Maciej Tuszynski <xtzl9j78@students.codeinstitute.net>
2024-12-19 13:05:16 +0000

    all files from desktop

--- assetstest/js/firstpage.js
+++
@@ -1,9 +0,0 @@
-const redirectBtn = document.getElementById('redirectBtn');
-const pageTransition = document.getElementById('pageTransition');
-
-redirectBtn.addEventListener('click', () => {
-    pageTransition.classList.add('fade-out');
-    setTimeout(() => {
-        window.location.href = "shop.html";
-    }, 1000); 
-});
